96b41c4ce983acb143a3b83afd231a08bad7c167
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-29 19:16:20 +0300

    Bind Event Listener

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+//function addingEventListener() {
+
+//const sinon = require('sinon');
+
+/*describe("index.js", () => {
+    let input;
+  
+    beforeEach(function() {
+      input = document.getElementById('input');
+      sinon.spy(input, 'addEventListener');
+    })
+  
+    it("binds an event listener in addingEventListener()", () => {
+      addingEventListener();
+      expect(input.addEventListener.called).to.be.true;
+    })*/
 function addingEventListener() {
+    const input = document.querySelector('#input')
+    function clickAlert(){
+        return alert("Clicked");
+
+    }
+
+
+    
+input.addEventListener('click', clickAlert)
 }
+
